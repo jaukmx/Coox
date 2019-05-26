4f0e980c99bf5dc0469538f23f2b4f4fffc72a58
jose uc <arturouc1997@gmail.com>
2019-05-25 22:26:01 -0500

    Actualizaciond de btn a TxtView para registrarse

--- app/src/main/java/com/example/arturo/coox/Login.java
+++ app/src/main/java/com/example/arturo/coox/Login.java
@@ -5,22 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import static com.example.arturo.coox.R.id.btn_registrar;
 
 public class Login extends AppCompatActivity {
     //variable textviw
-    Button btn_registrar;
+    TextView registrarse;
+    EditText username, userpassword;
+    Button btn_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //funcion de logearse
 
-        //
-
-        btn_registrar=findViewById(R.id.btn_registrar);
-        btn_registrar.setOnClickListener(new View.OnClickListener() {
+        //Funcion de pasar a paguina de registarse
+        registrarse=findViewById(R.id.txtRegistrar);
+        registrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intentReg = new Intent(Login.this, Registro.class);
--- app/src/main/java/com/example/arturo/coox/panel_padres.java
+++ app/src/main/java/com/example/arturo/coox/panel_padres.java
@@ -1,13 +1,19 @@
 package com.example.arturo.coox;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class panel_padres extends AppCompatActivity {
-
+    TextView txtUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panel_padres);
+        Intent intent =getIntent();
+        String user=intent.getStringExtra("username");
+        txtUser =findViewById(R.id.txtUser);
+        txtUser.setText("Bienvenido "+user);
     }
 }
