7b6659f72b9c25b5fac752d36fb63ba7c541d90a
jose uc <arturouc1997@gmail.com>
2019-05-26 01:44:49 -0500

    Version con login que se conecta a un servidor local PHP

--- app/src/main/java/com/example/arturo/coox/Login.java
+++ app/src/main/java/com/example/arturo/coox/Login.java
@@ -7,29 +7,102 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Login extends AppCompatActivity {
     //variable textviw
     TextView registrarse;
     EditText username, userpassword;
-    Button btn_login;
+    Button btnlogin;
+
+    private static final String loginUrl = "http://192.168.1.66/coox/login.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //funcion de logearse
+
 
         //Funcion de pasar a paguina de registarse
-        registrarse=findViewById(R.id.txtRegistrar);
+        registrarse = findViewById(R.id.txtRegistrar);
+        //variasbles del formulario para logearse
+        username = findViewById(R.id.txtUsuario);
+        userpassword = findViewById(R.id.txtContraseña);
+        btnlogin = findViewById(R.id.btn_login);
+
+//Funcion de pasar a paguina de registarse
         registrarse.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intentReg = new Intent(Login.this, Registro.class);
-                Login.this.startActivity(intentReg);
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Registro.class);
+                startActivity(intent);
             }
         });
-}
 
- }
+
+
+
+/////Boton Login
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                userlogin();
+            }
+        });
+
+
+    }
+    public void userlogin(){
+        StringRequest request =new StringRequest(StringRequest.Method.POST, loginUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(response.contains("success")){
+
+                    Intent intent = new Intent(getApplicationContext(), panel_padres.class);
+
+
+                    intent.putExtra("username", username.getText().toString());
+                    startActivity(intent);
+
+                }else{
+                    Toast.makeText(getApplicationContext(), "Usuario o contraseña invalido", Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("loginuser","true");
+                params.put("username",username.getText().toString().trim());
+                params.put("userpassword",userpassword.getText().toString().trim());
+                return params;
+
+
+            }
+        };
+        Volley.newRequestQueue(this).add(request);
+
+
+
+
+    }
+
+}
--- app/src/main/java/com/example/arturo/coox/panel_padres.java
+++ app/src/main/java/com/example/arturo/coox/panel_padres.java
@@ -6,14 +6,16 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class panel_padres extends AppCompatActivity {
-    TextView txtUser;
+    TextView txtUsuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panel_padres);
+
         Intent intent =getIntent();
+
         String user=intent.getStringExtra("username");
-        txtUser =findViewById(R.id.txtUser);
-        txtUser.setText("Bienvenido "+user);
+        txtUsuario =findViewById(R.id.txtUser);
+        txtUsuario.setText("Bienvenido "+user);
     }
 }
